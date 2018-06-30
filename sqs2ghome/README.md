901cc9187b468bd9abbed8dea25acc93cc0d9c84
sycq <shunsuke.yamabe.cq@hitachi-systems.com>
2018-06-30 17:20:49 +0900

    initial commit

---
+++ sqs2ghome/README.md
@@ -0,0 +1,72 @@
+# SQS2GoogleHome
+
+## Raspberry Pi のセットアップ
+
+* Raspbian Stretch Lite をDL  
+  https://www.raspberrypi.org/downloads/raspbian/
+
+* EtcherをDL＆インストール  
+  https://etcher.io/
+
+* Etcherでイメージ(zipのままでOK)を焼く
+
+* 焼いた後のSDカードのドライブを開き、最上位に`ssh`という名前のファイルを作成(空でOK)すると起動時にSSHができるようになる。
+
+* 同じく、`wpa_supplicant.conf`を作成して以下を記述すると起動時に自動的にWiFiに繋がる。
+
+> 改行コードは`LF`で。
+
+```
+country=JP
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+network={
+        ssid="----Your-WiFi-SSID----"
+        psk="----PLAIN-PASSPHRASE----"
+}
+```
+
+* Macはそのまま、Windowsなら[Bonjour](https://support.apple.com/kb/DL999?viewlocale=en_US&locale=en_US)がインストールしてあれば、raspberrypi.local でSSH接続できるはず。
+
+```
+pi / raspberry
+```
+
+## AWS SDK と Google Home Notifier を利用するためのセットアップ 
+
+* Nodeや関連パッケージのインストール  
+  > curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -  
+  > sudo apt-get install -y nodejs  
+  > sudo apt-get install -y git-core libnss-mdns libavahi-compat-libdnssd-dev  
+
+
+* プロジェクトの作成  
+  > mkdir XXX  
+  > cd XXX  
+  > npm init  
+  > npm install google-home-notifier aws-sdk --save
+
+* MNDSのソース修正(ラズパイ用バグ対策)  
+  > vi node_modules/mdns/lib/browser.js
+
+```
+Browser.defaultResolverSequence = [
+  rst.DNSServiceResolve(), 'DNSServiceGetAddrInfo' in dns_sd ? rst.DNSServiceGetAddrInfo() : rst.getaddrinfo()
+, rst.makeAddressesUnique()
+];
+
+↓
+
+Browser.defaultResolverSequence = [
+  rst.DNSServiceResolve(), 'DNSServiceGetAddrInfo' in dns_sd ? rst.DNSServiceGetAddrInfo() : rst.getaddrinfo({families:[4]})
+, rst.makeAddressesUnique()
+];
+```
+
+* 環境変数の設定  
+  > export AWS_ACCESS_KEY_ID=XXXXXXXXXX  
+  > export AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXX  
+    
+  > SQS:ReceiveMessage と SQS:DeleteMessage の権限が必要
+
+* ディレクトリにソースをコピーして実行する。
---
+++ sqs2ghome/main.js
@@ -0,0 +1,75 @@
+/**
+ Check SQS Queue and notify to google home on the local network if a new message is coming.
+
+ Requires EnvVar AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.
+ Their IAM has to have SQS:ReceiveMessage and SQS:DeleteMessage.
+
+  Refs
+  https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sqs-examples-send-receive-messages.html
+  https://github.com/noelportugal/google-home-notifier#after-npm-install
+  https://qiita.com/SatoTakumi/items/c9de7ff27e5b70508066
+*/
+
+const AWS = require('aws-sdk');
+const googlehome = require('google-home-notifier')
+
+// AWS settings
+const AWS_REGION = 'ap-northeast-1';
+const SQS_URL = 'https://sqs.ap-northeast-1.amazonaws.com/626062478346/sample-queue';
+const QUEUE_CHECK_INTVL_SEC = 1
+// Google Home settings
+const msg_tts = 'アレクサ、お客様がご来場でございます。';
+
+// Setup
+AWS.config.update({ region:  AWS_REGION});
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const queueURL = SQS_URL;
+const params = {
+    AttributeNames: [
+        'SentTimestamp'
+    ],
+    MaxNumberOfMessages: 1,
+    MessageAttributeNames: [
+        'All'
+    ],
+    QueueUrl: queueURL,
+    VisibilityTimeout: 0,
+    WaitTimeSeconds: 0
+};
+googlehome.device('Google-Home', 'ja');
+
+// Main part
+const receiveMsg = function () {
+    sqs.receiveMessage(params, function (err, data) {
+        if (err) {
+            // Failed to get SQS message.
+            console.log("Receive Error", err);
+            setTimeout(receiveMsg, QUEUE_CHECK_INTVL_SEC * 1000);
+        } else if (data.Messages) {
+            // Got message. Notify Google Home.
+            console.log(data.Messages)
+            googlehome.notify(msg_tts, function (res) {
+                console.log(res);
+                // Delete SQS message after notifying Google Home.
+                const deleteParams = {
+                    QueueUrl: queueURL,
+                    ReceiptHandle: data.Messages[0].ReceiptHandle
+                };
+                sqs.deleteMessage(deleteParams, function (err, data) {
+                    if (err) {
+                        console.log("Delete Error", err);
+                    } else {
+                        console.log("Message Deleted", data);
+                    }
+                });
+                // Repeat to wait for another message.
+                setTimeout(receiveMsg, QUEUE_CHECK_INTVL_SEC * 1000);
+            })
+        } else {
+            // No message. Repeat to wait for another message.
+            setTimeout(receiveMsg, QUEUE_CHECK_INTVL_SEC * 1000);
+        }
+    })
+};
+
+receiveMsg()
